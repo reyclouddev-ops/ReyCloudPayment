992c37eda5682b2b40ab771b67c4876508af730b
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-24 00:01:28 +0700

    Update midtrans.js

--- api/webhook/midtrans.js
+++ api/webhook/midtrans.js
@@ -1,22 +1,35 @@
 const crypto = require("crypto");
 
 async function sendTelegram(text) {
+async function sendTelegramPhoto(caption){
+
+    const photo =
+    "https://reycloudpayment.legionteknologi.my.id/success.jpg";
+
     const response = await fetch(
-        `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
+        `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendPhoto`,
         {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
             },
-            body: JSON.stringify({
-                chat_id: process.env.TELEGRAM_CHAT_ID,
-                text,
-                parse_mode: "HTML"
+            body:JSON.stringify({
+
+                chat_id:
+                process.env.TELEGRAM_CHAT_ID,
+
+                photo,
+
+                caption,
+
+                parse_mode:"HTML"
+
             })
         }
     );
 
     return response.json();
+
 }
 
 module.exports = async (req, res) => {
@@ -52,26 +65,36 @@ module.exports = async (req, res) => {
         const nominal = Number(data.gross_amount).toLocaleString("id-ID");
         const metode = data.payment_type;
 
-        await sendTelegram(`
-<b>🚀 REYCLOUD PAYMENT</b>
+        await sendTelegramPhoto(`
+🚀 <b>REYCLOUD PAYMENT INFO</b>
 
-🧾 Order ID:
+━━━━━━━━━━━━━━━━━━━━━━
+
+${status}
+
+━━━━━━━━━━━━━━━━━━━━━━
+
+🧾 <b>Order ID</b>
 <code>${data.order_id}</code>
 
-💰 Nominal:
+💰 <b>Nominal</b>
 Rp${nominal}
 
-💳 Metode:
-${metode}
+💳 <b>Metode Pembayaran</b>
+${data.payment_type}
 
-📊 Status:
+📊 <b>Status</b>
 ${data.transaction_status.toUpperCase()}
 
-📅 Waktu:
-${data.transaction_time}
+📅 <b>Waktu Transaksi</b>
+${waktu}
 
-🆔 Transaction ID:
+🆔 <b>Transaction ID</b>
 <code>${data.transaction_id}</code>
+
+━━━━━━━━━━━━━━━━━━━━━━
+
+🤖 <b>ReyCloud Payment Gateway</b>
 `);
 
         return res.status(200).json({
